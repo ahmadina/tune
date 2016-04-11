8fad23808e90e9a0c540953e20cfb1d5d9b05361
Ahmadina <me@ahmadina.com>
2016-04-11 11:43:05 +0430

    e2e and unit test

---
+++ test/unit/index.js
@@ -0,0 +1,17 @@
+// Polyfill fn.bind() for PhantomJS
+/* eslint-disable no-extend-native */
+Function.prototype.bind = require('function-bind')
+
+// require all test files (files that ends with .spec.js)
+var testsContext = require.context('./specs', true, /\.spec$/)
+testsContext.keys().forEach(testsContext)
+
+// require all src files except main.js for coverage.
+// you can also change this to match only the subset of files that
+// you want coverage for.
+var clientContext = require.context('../../client', true, /^\.\/(?!main(\.js)?$)/)
+clientContext.keys().forEach(clientContext)
+
+
+var serverContext = require.context('../../server', true, /^\.\/(?!main(\.js)?$)/)
+serverContext.keys().forEach(serverContext)
